d764ebc87c2757ff3c989f3cfe9b8d4da0502e75
Andres Ruiz Graciano <aandres.r98@gmail.com>
2020-04-25 15:19:05 -0500

    model component finished

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,11 @@ const router = new Router();
 const brands = require('./components/brand/brandRoute');
 const pointsOfSale = require('./components/pointOfSale/pointOfSaleRoute');
 const jobs = require('./components/job/jobRoute');
+const models = require('./components/model/modelRoute');
 
 router.use('/brands', brands);
 router.use('/pos', pointsOfSale);
 router.use('/jobs', jobs);
+router.use('/models', models);
 
 module.exports = router;
