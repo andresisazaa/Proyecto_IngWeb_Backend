6cb734798d34607064fab2078bfda5d65b84b9ac
Andres Ruiz Graciano <aandres.r98@gmail.com>
2020-04-25 16:25:01 -0500

    add: get purchases and get purchase by id

--- app/routes.js
+++ app/routes.js
@@ -6,10 +6,12 @@ const brands = require('./components/brand/brandRoute');
 const pointsOfSale = require('./components/pointOfSale/pointOfSaleRoute');
 const jobs = require('./components/job/jobRoute');
 const models = require('./components/model/modelRoute');
+const purchases = require('./components/purchase/purchaseRoute');
 
 router.use('/brands', brands);
 router.use('/pos', pointsOfSale);
 router.use('/jobs', jobs);
 router.use('/models', models);
+router.use('/purchases', purchases);
 
 module.exports = router;
